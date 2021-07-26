c346d56cfb7731e01d954319ec943d975c91a471
Alicia AyersLott <amayers92@gmail.com>
2021-07-26 13:29:24 -0500

    added event listener to products list

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,6 +1,31 @@
 import { getEmployees } from "./database.js"
+import { Products } from "./Products.js"
 
 const employees = getEmployees()
+
+// document.addEventListener(
+//     "click",
+//     (clickEvent) => {
+//         const itemClicked = clickEvent.target
+//         if (itemClicked.id.startsWith("employee")) {
+//             const [, employeeId] = itemClicked.id.split("--")
+
+//             for (const employee of employees) {
+//                 if (employee.Id === parseInt(employeeId)) {
+
+//                     const employeeOrders = orders.filter(
+//                         (order) => {
+//                             if (order.employeeID === employee.id) {
+//                                 return true
+//                             }
+//                         }
+//                     )
+//                     window.alert(`${employee.name} sold ${} products `)
+//                 }
+//             }
+//         }
+//     }
+// )
 
 export const Employees = () => {
     let html = "<ul>"
