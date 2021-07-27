0c12a03526a0cf4fd84c706dbbffc4d5db40c11b
Alicia AyersLott <amayers92@gmail.com>
2021-07-26 21:13:36 -0500

    final code with event listeners

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,37 +1,36 @@
 import { getEmployees } from "./database.js"
 import { Products } from "./Products.js"
+import { getOrders } from "./database.js"
 
 const employees = getEmployees()
 
-// document.addEventListener(
-//     "click",
-//     (clickEvent) => {
-//         const itemClicked = clickEvent.target
-//         if (itemClicked.id.startsWith("employee")) {
-//             const [, employeeId] = itemClicked.id.split("--")
+ document.addEventListener(
+     "click",
+     (clickEvent) => {
+         const itemClicked = clickEvent.target
+             const [, employeeId] = itemClicked.id.split("--")
 
-//             for (const employee of employees) {
-//                 if (employee.Id === parseInt(employeeId)) {
-
-//                     const employeeOrders = orders.filter(
-//                         (order) => {
-//                             if (order.employeeID === employee.id) {
-//                                 return true
-//                             }
-//                         }
-//                     )
-//                     window.alert(`${employee.name} sold ${} products `)
-//                 }
-//             }
-//         }
-//     }
-// )
+             for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+                    const orders = getOrders()
+                    const employeeOrders = orders.filter(
+                        (order) => {
+                            if (order.employeeId === employee.id) {
+                               return true
+                             }
+                         }
+                     ).length
+                     window.alert(`${employee.name} sold ${employeeOrders} products `)
+                }
+             }
+         }
+)
 
 export const Employees = () => {
     let html = "<ul>"
 
     for (const employee of employees) {
-        html += `<li id="employee--${employee.Id}">${employee.name}</li>` // added a . between employee and ID for property
+        html += `<li id="employee--${employee.id}">${employee.name}</li>` // added a . between employee and ID for property
     }
 
     html += "</ul>"
